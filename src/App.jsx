675c4e12a7d302f771d7acea734b3b1fc4dae8bb
rf19990202 <152494023+rf19990202@users.noreply.github.com>
2024-05-05 23:38:01 +0800

    Add react aria component

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,8 @@ import { useState } from 'react';
 import User from './pages/user';
 import Chat from './pages/chat';
 import Header from './components/header';
-import api from '@/api/index';
 
 function App() {
-  // const [user, setuser] = useUser(null);
-  // console.log(user);
-  async function getUser() {
-    var res = await api.getuser(1);
-    console.log(res);
-  }
-
   var [location, setlocation] = useState(<User></User>);
   return (
     <>
@@ -24,7 +16,6 @@ function App() {
         {location === 'user' && <User></User>}
         {location === 'chat' && <Chat></Chat>}
       </main>
-      <footer onClick={getUser}>rf@@ 2024 ALL RIGHTS RESERVED</footer>
     </>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,10 @@
+import {Link} from 'react-aria-components';
 // eslint-disable-next-line react/prop-types
 function Header({ setlocation }) {
   return (
     <>
-      <p onClick={() => setlocation('user')}>user</p>
-      <p onClick={() => setlocation('chat')}>chat</p>
+      <Link onClick={() => setlocation('user')}>user</Link>
+      <Link onClick={() => setlocation('chat')}>chat</Link>
     </>
   );
 }
