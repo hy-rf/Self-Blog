9701cd03163045ec6843c5cc5f383c051fec4584
hy-rf <heyu880202@gmail.com>
2024-05-11 10:59:38 +0800

    Change header child width

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,29 @@
-import { Link } from 'react-aria-components';
 // eslint-disable-next-line react/prop-types
 function Header({ setlocation }) {
   return (
     <>
-      <div onClick={() => setlocation('post')}>logo</div>
+      <div
+        onClick={() => {
+          setlocation('post');
+        }}
+      >
+        logo
+      </div>
       <div id="search-bar">searchbar</div>
-      <Link onPress={() => setlocation('user')}>user</Link>
-      <Link onPress={() => setlocation('chat')}>chat</Link>
+      <div
+        onClick={() => {
+          setlocation('user');
+        }}
+      >
+        user
+      </div>
+      <div
+        onClick={() => {
+          setlocation('chat');
+        }}
+      >
+        chat
+      </div>
     </>
   );
 }
