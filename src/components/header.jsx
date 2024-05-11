24dbaa0e4be89ddae037ce91633cf70074097410
hy-rf <heyu880202@gmail.com>
2024-05-11 10:19:20 +0800

    Breaking Change! mobile first design
    default style is now mobile

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,10 @@
 import { Link } from 'react-aria-components';
-import './header.css';
 // eslint-disable-next-line react/prop-types
 function Header({ setlocation }) {
   return (
     <>
       <div onClick={() => setlocation('post')}>logo</div>
-      <div>searchbar</div>
+      <div id="search-bar">searchbar</div>
       <Link onPress={() => setlocation('user')}>user</Link>
       <Link onPress={() => setlocation('chat')}>chat</Link>
     </>
