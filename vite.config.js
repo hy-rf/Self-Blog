67be1f84bf3761ff6180b93bbd848ded843b1585
hy-rf <heyu880202@gmail.com>
2024-05-14 17:55:25 +0800

    Add admin page

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,14 @@ const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        admin: path.resolve(__dirname, 'admin.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
