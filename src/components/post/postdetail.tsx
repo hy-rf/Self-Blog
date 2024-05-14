0c410f51ebfb4d9de05a3fd299157cd2d65ebaef
hy-rf <heyu880202@gmail.com>
2024-05-14 21:34:28 +0800

    Add ts css module support

--- src/components/post/postdetail.tsx
+++ src/components/post/postdetail.tsx
@@ -1,5 +1,6 @@
 import style from './postdetail.module.css';
-function PostDetail({ post, setCurrentOpenedPost }) {
+import { type Post } from '@/types/post';
+function PostDetail(post: Post, setCurrentOpenedPost: any) {
   return (
     <div className={style.container}>
       <button onClick={() => setCurrentOpenedPost(null)}>close</button>
