026bfeb365f23f650d0a784a021f05cce7b595d2
hy-rf <heyu880202@gmail.com>
2024-05-20 20:08:01 +0800

    refactor: Update Header component with styled-components and react-device-detect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,36 @@
+import styled from 'styled-components';
+import { isMobile } from 'react-device-detect';
+const HeaderStyle = isMobile
+  ? styled.header`
+      grid-area: header;
+      background: linear-gradient(
+        rgba(128, 128, 128, 0.4),
+        rgba(128, 128, 128, 0.2)
+      );
+      border-bottom: 1px solid rgba(128, 128, 128, 0.2);
+      padding: 5px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    `
+  : styled.header`
+      grid-area: header;
+      background: linear-gradient(
+        rgba(128, 128, 128, 0.4),
+        rgba(128, 128, 128, 0.2)
+      );
+      border-bottom: 1px solid rgba(128, 128, 128, 0.2);
+      padding: 5px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      display: none;
+    `;
 function Header() {
   return (
-    <header
-      style={{
-        gridArea: 'header',
-      }}
-    >
+    <HeaderStyle>
       <p>Example Header</p>
-    </header>
+    </HeaderStyle>
   );
 }
 
