f9bb29c123ff58a2df2758b6b847ab854f8b8e38
hy-rf <heyu880202@gmail.com>
2024-05-21 04:57:38 +0800

    Add z-index for nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,6 +10,7 @@ const NavStyle = isMobile
         rgba(128, 128, 128, 0.4),
         rgba(128, 128, 128, 0.2)
       );
+      z-index: 1;
       padding: 5px;
       border-right: 1px solid grey;
       & > * {
