b3e2d96d3f96a549e49593786c815c67426b77e4
hy-rf <heyu880202@gmail.com>
2024-05-21 22:25:08 +0800

    refactor: Update Header component with MUI styles and add search functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,86 @@
-import styled from 'styled-components';
-import { isMobile } from 'react-device-detect';
-const HeaderStyle = isMobile
-  ? styled.header`
-      grid-area: header;
-      background: linear-gradient(
-        rgba(128, 128, 128, 0.4),
-        rgba(128, 128, 128, 0.2)
-      );
-      border-bottom: 1px solid rgba(128, 128, 128, 0.2);
-      padding: 5px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-    `
-  : styled.header`
-      grid-area: header;
-      background: linear-gradient(
-        rgba(128, 128, 128, 0.4),
-        rgba(128, 128, 128, 0.2)
-      );
-      border-bottom: 1px solid rgba(128, 128, 128, 0.2);
-      padding: 5px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      display: none;
-    `;
-function Header() {
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
+import { ClickAwayListener } from '@mui/base';
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  width: '70%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const StyledSearchIconBase = styled(SearchIcon)(() => ({
+  position: 'absolute',
+  color: 'inherit',
+  width: '10%',
+  right: '1rem',
+}));
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '80%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '18ch',
+      '&:focus': {
+        width: '18ch',
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  const [showSearchBar, setShowSearchBar] = useState(false);
+  const handleShowSearchBar = () => {
+    if (showSearchBar) {
+      document.querySelector('main').style.filter = 'none';
+      document.querySelector('nav').style.filter = 'none';
+    } else {
+      document.querySelector('main').style.filter = 'blur(5px)';
+      document.querySelector('nav').style.filter = 'blur(5px)';
+    }
+    showSearchBar ? setShowSearchBar(false) : setShowSearchBar(true);
+  };
   return (
-    <HeaderStyle>
-      <p>Example Header</p>
-    </HeaderStyle>
+    <ClickAwayListener
+      onClickAway={() => {
+        if (!showSearchBar) return;
+        setShowSearchBar(false);
+        document.querySelector('main').style.filter = 'none';
+        document.querySelector('nav').style.filter = 'none';
+      }}
+    >
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            {showSearchBar && (
+              <Search>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </Search>
+            )}
+            <StyledSearchIconBase onClick={handleShowSearchBar} />
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </ClickAwayListener>
   );
 }
-
-export default Header;
